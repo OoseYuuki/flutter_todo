cddd24128f389fc1bff3919ed02477322381e51d
yuuki.oose <yuuki.oose@arsaga.jp>
2023-08-06 16:34:18 +0900

    Todoクラスを作成

---
+++ lib/todo.dart
@@ -0,0 +1,50 @@
+class Todo {
+  // ID
+  late int id;
+  // タイトル
+  late String title;
+
+  // 詳細
+  late Sring detail;
+
+  // 完了・未完了
+  late String done;
+
+  // 作成日時
+  late String createDate;
+
+  // 更新日時
+  late String updateDate;
+
+  // コンストラクタ
+  Todo(
+    this.id,
+    this.title,
+    this.detail,
+    this.done,
+    this.createDate,
+    this.updateDate,
+  );
+
+  // TodoモデルをMapに変換する。（保存時に使用）
+  Map toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'detail': detail,
+      'done': done,
+      'createDate': createDate,
+      'updateDate': updateDate,
+    };
+  }
+
+  // MapをTodoモデルに変換する。（読み込み時に使用）
+  Todo.fromJson(Map json) {
+    id = json['id'];
+    title = json['title'];
+    detail = json['detail'];
+    done = json['done'];
+    createDate = json['createDate'];
+    updateDate = json['updateDate'];
+  }
+}
